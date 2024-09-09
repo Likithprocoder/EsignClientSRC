79d4cd5028e9ce0616201827eef3ea2d5765bfd8
LikithM <likithm@integramicro.co.in>
2024-09-09 12:56:23 +0530

    09-09-24 Mail and sms

---
+++ build - Exit From Corp/static/js/99.e4fa4c2b.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonp@coreui/coreui-free-react-admin-template"]=this["webpackJsonp@coreui/coreui-free-react-admin-template"]||[]).push([[99],{1268:function(e,t,n){"use strict";n.r(t),n.d(t,"default",(function(){return c}));var r=n(1),a=n.n(r);class c extends a.a.Component{render(){return a.a.createElement("div",null,a.a.createElement("h1",null,"Document related details will be displayed soon...."))}}}}]);
+//# sourceMappingURL=99.e4fa4c2b.chunk.js.map
