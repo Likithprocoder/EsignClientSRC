7303d200326ecb3dea7b08c9c2d8579d25ac7450
abhijeet-m-integra <abhijeetm@integramicro.co.in>
2024-09-09 10:33:57 +0530

    Voucher based regsitation changes +fixes - 09092024

--- src/containers/URLConstant.js
+++ src/containers/URLConstant.js
@@ -1,14 +1,14 @@
 
-const HOST = "/MYSIGN/";
-var paymenturl = "/PAYMENTSIMULATOR/getunitscalltesting";
+// const HOST = "/MYSIGN/";
+// var paymenturl = "/PAYMENTSIMULATOR/getunitscalltesting";
 
 const { UpdateRounded } = require("@material-ui/icons");
 
 // const HOST = "https://docuxecstage.finfotech.co.in/MYSIGN/";
 // var paymenturl = "https://docuxecstage.finfotech.co.in/PAYMENTSIMULATOR/getunitscalltesting";
 
-// const HOST = "http://localhost:8090/MYSIGN/";
-// var paymenturl = "http://localhost:8095/PAYMENTSIMULATOR/getunitscalltesting";
+const HOST = "http://localhost:8090/MYSIGN/";
+var paymenturl = "http://localhost:8095/PAYMENTSIMULATOR/getunitscalltesting";
 
 
 const footerContent =
@@ -149,6 +149,7 @@ let URL = {
   getCorpEmployee: HOST+"getCorpEmployee",
   deleteCorpEmpMapping: HOST+"deleteCorpEmpMapping",
   updateCorpEmpMapping: HOST+"updateCorpEmpMapping",
+  deleteUserFromCorpGroup:HOST+"deleteUserFromCorpGroup",
 };
 
 module.exports = { URL };
